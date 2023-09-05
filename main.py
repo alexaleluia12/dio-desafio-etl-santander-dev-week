91375e69347c6cc157f6788ed43a192abcf974ae
alexaleluia12 <alexdiasaleluia@gmail.com>
2023-09-04 21:20:42 -0300

    conclusao load, melhora do readme

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from extract import read_imputs
-from transform import clients_from
-import pandas as pd
+from transform import clients_from, format_client
+
+import json
 
 if __name__ == "__main__":
-    # agrupamento feitos
+    # EXTRACT
     files = read_imputs()
     clients = clients_from(files)
-    print(clients)
-    """
-    df = pd.read_csv('./files/clientes.csv', quotechar="'")
-    # print(df.head(10))
-    print(df['id'].unique().tolist())
-    print()
-    print(df['endereco'])
-    """
+    group_clients = []
+
+    # TRANSFORM
+    for client in clients:
+        client_json_like = format_client(clients[client])
+        group_clients.append(client_json_like)
+
+    # LOAD
+    with open('saida/db.json', mode='w') as file:
+        json.dump(group_clients, file)
+        print('ETL feito com sucesso')
